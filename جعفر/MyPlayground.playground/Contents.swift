0de1296111f3263c1f36dd25526d069eae9d6c86
jafar114 <66698691+jafar114@users.noreply.github.com>
2020-06-13 22:32:53 +0300

    finesh clase worke

---
+++ جعفر/MyPlayground.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let myName = "jafar"
+
+var hobby1 = "football"
+
+var hobby2 = "handball"
+
+var age = 16
+
+var wight = 45
+
+let frindeName = "jafar"
+
+var frindeHobby1  = "football"
+
+var frindeHobby2  = "handball"
+
+var frindeAge = 16
+
+var frindeWight = 45
+
+print ("hello,playgrounde")
+print ( "myName", "Is", myName, "myHobby1", "is", hobby1)
+print ("myHobby2", "is", hobby2, "myAge", "is", age,"myWight", "is", wight)
+
+print ("myFrindename", "is", frindeName)
+print ("myFrindeage","is",age, "myFrindewight", "is" ,wight,"myfrindehobby1", "is", hobby1)
+print ("myFrindehobby2", "is", hobby2)
+
+myName == frindeName
+age == frindeAge
+wight == frindeWight
+hobby1 == frindeHobby1
+hobby2 == frindeHobby2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
